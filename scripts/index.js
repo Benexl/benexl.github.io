ed2b726964bf74cd8ab6fe8b69eb4441ab2ae774
Benexl <benextempest@gmail.com>
2025-03-09 12:40:46 +0300

    feat: implement image viewer modal for enhanced image display

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
-document.addEventListener("mousemove", (e) => {
-  let x = (e.clientX / window.innerWidth) * 100;
-  let y = (e.clientY / window.innerHeight) * 100;
+$("img").click(function () {
+  $("#full-image").attr("src", $(this).attr("src"));
+  $("#image-viewer").show();
+});
 
-  document.body.style.background = `radial-gradient(circle at ${x}% ${y}%, 
-        rgba(255, 0, 150, 0.6), 
-        rgba(0, 255, 255, 0.4), 
-        rgba(0, 0, 0, 0.9))`;
+$("#image-viewer .close").click(function () {
+  $("#image-viewer").hide();
 });

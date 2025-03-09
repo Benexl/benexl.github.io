09864636955625e0f6ab3ca793f974e06d797060
Benexl <benextempest@gmail.com>
2025-03-09 13:58:20 +0300

    feat: enhance image viewer with navigation controls and fix data attributes

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 const designs = {
   adia: {
     desktop: [
-      "./assets/images/adia/home.png",
-      "./assets/images/adia/desktop/blog.png",
       "./assets/images/adia/desktop/home.png",
+      "./assets/images/adia/desktop/blog.png",
       "./assets/images/adia/desktop/write.png",
     ],
     phone: [
-      "./assets/images/adia/phone/blog.png",
       "./assets/images/adia/phone/home.png",
+      "./assets/images/adia/phone/blog.png",
       "./assets/images/adia/phone/write.png",
     ],
   },
   graphix: {
     desktop: [
+      "./assets/images/graphix/desktop/Home.png",
       "./assets/images/graphix/desktop/Bin.png",
       "./assets/images/graphix/desktop/About.png",
       "./assets/images/graphix/desktop/Development Resources.png",
@@ -21,7 +21,6 @@ const designs = {
       "./assets/images/graphix/desktop/from files.png",
       "./assets/images/graphix/desktop/from folder.png",
       "./assets/images/graphix/desktop/help.png",
-      "./assets/images/graphix/desktop/Home.png",
       "./assets/images/graphix/desktop/Projects.png",
       "./assets/images/graphix/desktop/reportIssue.png",
       "./assets/images/graphix/desktop/Settings.png",
@@ -45,6 +44,39 @@ const designs = {
 
 $("[data-design]").click(function () {
   $("#full-image").attr("src", $(this).attr("src"));
+  const currentDesignEl = this;
+
+  $("#next-image")
+    .off("click")
+    .on("click", function () {
+      let currentImage = $("#full-image").attr("src");
+      let currentDesign = $(currentDesignEl).attr("data-design");
+      console.log(currentDesign);
+      let currentIndex =
+        designs[currentDesign]["desktop"].indexOf(currentImage);
+      if (currentIndex < designs[currentDesign]["desktop"].length - 1) {
+        $("#full-image").attr(
+          "src",
+          designs[currentDesign]["desktop"][currentIndex + 1]
+        );
+      }
+    });
+
+  $("#previous-image")
+    .off("click")
+    .on("click", function () {
+      let currentImage = $("#full-image").attr("src");
+      let currentDesign = $(currentDesignEl).attr("data-design");
+      let currentIndex =
+        designs[currentDesign]["desktop"].indexOf(currentImage);
+      if (currentIndex > 0) {
+        $("#full-image").attr(
+          "src",
+          designs[currentDesign]["desktop"][currentIndex - 1]
+        );
+      }
+    });
+
   $("#image-viewer").show();
 });
 
